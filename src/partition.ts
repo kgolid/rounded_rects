9baa7e601c945c899b8355c81a3108817e939c64
Kjetil Midtgarden Golid <kjetil.golid@gmail.com>
2025-09-26 14:33:25 +0200

    reflective partition

--- src/partition.ts
+++ src/partition.ts
@@ -9,7 +9,7 @@ const PAD_RATIO = 0; //0.0002; //0.008;
 const terminal_chance: (d: number) => number = (d) => (d - 4) / 20;
 
 export function create_supergrid() {
-  let grid: PartitionCell[] = [cell(vec(-0.5, -0.5), vec(1, 1, 0), 0)];
+  let grid: PartitionCell[] = [cell(vec(-0.5, -0.5), vec(1, 1, 0), 0, false)];
 
   let splitted = divide_cells_repeatedly(grid, 0);
   return splitted;
@@ -34,15 +34,15 @@ function divide_cells_repeatedly(cells: PartitionCell[], iteration: number) {
 
   let pick_id = my_shuffle(Array.from(ids))[0];
 
-  let picked_cells = cells
-    .filter((c) => c.id == pick_id)
-    .map((c) => pad_cell(c, c.depth < 9 ? PAD_RATIO * Math.pow(c.depth, 2) : 0));
+  let picked_cells = cells.filter((c) => c.id == pick_id);
+  //   .map((c) => pad_cell(c, c.depth < 9 ? PAD_RATIO * Math.pow(c.depth, 2) : 0));
   let unpicked_cells = cells.filter((c) => c.id != pick_id);
 
   let divided_cells = divide_cell(picked_cells[0], iteration);
+  let reflected_divided_cells = divided_cells.map((dc) => reflect_cell(dc, picked_cells[0]));
 
   let new_cells = picked_cells.flatMap((pc) =>
-    divided_cells.map((dc) => cell(add(pc.pos, dc.pos), dc.dim, dc.depth, dc.terminal, dc.id))
+    (pc.reflected ? reflected_divided_cells : divided_cells).map((dc) => ({ ...dc, pos: add(dc.pos, pc.pos) }))
   );
 
   // let picks_size = Math.ceil(ids.size / 2);
@@ -92,7 +92,7 @@ function slice_cell(c: PartitionCell, pad: number): PartitionCell[] {
       let pos = vec(pad_x / 2 + i * cell_width, pad_y / 2 + j * cell_height, 0);
       let dim = vec(cell_width - pad_x, cell_height - pad_y, c.dim.z);
 
-      let new_cell = cell(pos, dim, depth, terminal, id);
+      let new_cell = cell(pos, dim, depth, c.reflected, terminal, id);
       cells.push(new_cell);
     }
   }
@@ -114,8 +114,10 @@ function split_cell(c: PartitionCell, pad: number): PartitionCell[] {
 }
 
 function split_cell_horisontally(c: PartitionCell, pad: number): PartitionCell[] {
-  let r1 = (Math.random() + Math.random() + Math.random()) / 3;
-  let r = 0.1 + r1 * 0.8;
+  let reflect = Math.random() < 0.2; //c.depth < 2;
+
+  let r1 = (Math.random() + Math.random()) / 2;
+  let r = reflect ? 0.5 : 0.1 + r1 * 0.8;
 
   let split_y = c.dim.y * r;
 
@@ -126,18 +128,31 @@ function split_cell_horisontally(c: PartitionCell, pad: number): PartitionCell[]
   let depth = c.depth + 1;
   let terminal = Math.random() < terminal_chance(depth);
 
-  let c1 = cell(nullVector(), vec(c.dim.x, split_y - pad, h1), depth, terminal);
+  let id = generate_cell_id();
 
-  let c2 = cell(vec(0, split_y + pad, 0), vec(c.dim.x, c.dim.y - split_y - pad, h2), depth, terminal);
+  let c1 = cell(nullVector(), vec(c.dim.x, split_y - pad, 0), depth, c.reflected, terminal, reflect ? id : -1);
 
-  console.log(c.id + ' -h-> ' + c1.id + ', ' + c2.id);
+  let c2 = cell(
+    vec(0, split_y + pad, 0),
+    vec(c.dim.x, c.dim.y - split_y - pad, 0),
+    depth,
+    c.reflected != reflect,
+    terminal,
+    reflect ? id : -1
+  );
+
+  let arrow = reflect ? ' -hr-> ' : ' -h--> ';
+
+  console.log(c.id + arrow + c1.id + ', ' + c2.id + ' (' + r + ')');
 
   return [c1, c2];
 }
 
 function split_cell_vertically(c: PartitionCell, pad: number): PartitionCell[] {
+  let reflect = Math.random() < 0.2; //c.depth < 2;
+
   let r1 = (Math.random() + Math.random() + Math.random()) / 3;
-  let r = 0.1 + r1 * 0.8;
+  let r = reflect ? 0.5 : 0.1 + r1 * 0.8;
 
   let split_x = c.dim.x * r;
 
@@ -148,21 +163,47 @@ function split_cell_vertically(c: PartitionCell, pad: number): PartitionCell[] {
   let depth = c.depth + 1;
   let terminal = Math.random() < terminal_chance(depth);
 
-  let c1 = cell(nullVector(), vec(split_x - pad, c.dim.y, h1), depth, terminal);
+  let id = generate_cell_id();
 
-  let c2 = cell(vec(split_x + pad, 0, 0), vec(c.dim.x - split_x - pad, c.dim.y, h2), depth, terminal);
-  console.log(c.id + ' -v-> ' + c1.id + ', ' + c2.id);
+  let c1 = cell(nullVector(), vec(split_x - pad, c.dim.y, 0), depth, c.reflected, terminal, reflect ? id : -1);
+
+  let c2 = cell(
+    vec(split_x + pad, 0, 0),
+    vec(c.dim.x - split_x - pad, c.dim.y, 0),
+    depth,
+    c.reflected != reflect,
+    terminal,
+    reflect ? id : -1
+  );
+
+  let arrow = reflect ? ' -vr-> ' : ' -v--> ';
+
+  console.log(c.id + arrow + c1.id + ', ' + c2.id + ' (' + r + ')');
 
   return [c1, c2];
 }
 
-function pad_cell(c: PartitionCell, pad: number) {
-  return cell(add(c.pos, vec(pad, pad)), sub(c.dim, vec(pad * 2, pad * 2)), c.depth, c.terminal, c.id);
+function reflect_cell(c: PartitionCell, container: PartitionCell) {
+  let new_x_pos = container.dim.x - c.dim.x - c.pos.x;
+  let new_y_pos = container.dim.y - c.dim.y - c.pos.y;
+
+  return cell(vec(new_x_pos, new_y_pos), c.dim, c.depth, !c.reflected, c.terminal, c.id);
 }
 
-function cell(pos: Vec, dim: Vec, depth: number, terminal: boolean = false, id: number = -1): PartitionCell {
+function pad_cell(c: PartitionCell, pad: number) {
+  return cell(add(c.pos, vec(pad, pad)), sub(c.dim, vec(pad * 2, pad * 2)), c.depth, c.reflected, c.terminal, c.id);
+}
+
+function cell(
+  pos: Vec,
+  dim: Vec,
+  depth: number,
+  reflected: boolean,
+  terminal: boolean = false,
+  id: number = -1
+): PartitionCell {
   if (id == -1) id = generate_cell_id();
   //let terminal = Math.random() < (depth - 5) / 15;
 
-  return { pos, dim, id, depth, terminal };
+  return { pos, dim, id, depth, reflected, terminal };
 }
